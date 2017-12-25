77ff5941a977f80babb8d265266fc5697b24657b
Miguel Zetina <Loefthur26@gmail.com>
2017-12-25 12:27:08 -0600

    Problema 0005: Factorización pendiente, tarda mucho.

---
+++ p0005.py
@@ -0,0 +1,29 @@
+"""
+Problem 5: Smallest multiple
+2520 is the smallest number that can be divided by each of the numbers from
+1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the
+numbers from 1 to 20?
+"""
+
+
+def number_divisible(num, max, min=1):
+    for n in range(min, max):
+        if num % n != 0:
+            return False
+    return True
+
+
+def smallest_multiple(max, min=1):
+    num = 0
+    while True:
+        num += 1
+        print(num)
+        if number_divisible(num, max, min):
+            print("El número {} es divisible total".format(num))
+            break
+    print('El número más chico que es posible dividir con todos los números de {} a {} es {}'
+          .format(min, max, num))
+
+
+smallest_multiple(10, 1)
